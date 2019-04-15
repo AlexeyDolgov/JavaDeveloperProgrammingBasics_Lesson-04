e5c40f0899e963de42ef9a5902ae76e03bd02c3b
Alexey Dolgov <bifourcator@gmail.com>
2019-04-15 17:02:52 +0300

    Programming Basics Lesson 4 done!

---
+++ JavaDeveloperProgrammingBasics_Lesson-04/task4_1/task4_1.js
@@ -0,0 +1,26 @@
+﻿function checkAge(age) {
+	if (age > 18) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+// Используя тернарный оператор ?
+
+function checkAge2(age) {
+	return (age > 18) ? true : false;
+}
+
+// Используя оператор ||
+
+function checkAge3(age) {
+	return (age > 18) || false;
+}
+
+console.log(checkAge(16));
+console.log(checkAge2(16));
+console.log(checkAge3(16));
+console.log(checkAge(21));
+console.log(checkAge2(21));
+console.log(checkAge3(21));
---
+++ JavaDeveloperProgrammingBasics_Lesson-04/task4_3/task4_3.js
@@ -0,0 +1,11 @@
+﻿function pow(x, n) {
+	let a = 1;
+	for (let i = 1; i <= n; i++) {
+		a = a * x;
+	}
+	return a;
+}
+
+console.log(pow(3, 2));
+console.log(pow(3, 3));
+console.log(pow(1, 100));
